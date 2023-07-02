634251c0b0d23506954dff2bf61b1d69f44d63ac
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-07-02 14:47:48 -0500

    feat: add new wallets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,76 @@
 import '../styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
-import { lightTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme
+} from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import celoGroups from '@celo/rainbowkit-celo/lists'
-import { Alfajores, Celo  } from '@celo/rainbowkit-celo/chains'
-import '@rainbow-me/rainbowkit/styles.css'
 
-const projectId = 'your-wallet-connnect-project-id' // get one at https://cloud.walletconnect.com/app
+// Import known recommended wallets
+import { Valora, CeloWallet, CeloTerminal } from '@celo/rainbowkit-celo/wallets'
+import {
+  metaMaskWallet,
+  walletConnectWallet,
+  omniWallet,
+  ledgerWallet,
+  coinbaseWallet,
+} from '@rainbow-me/rainbowkit/wallets'
+
+// Import CELO chain information
+import { Alfajores, Celo } from '@celo/rainbowkit-celo/chains'
+
+const projectId = '7e527e8d641d036dca61031d4bb8b5bc'
 
 const { chains, publicClient } = configureChains(
   [Alfajores, Celo],
   [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default.http[0] }) })]
 )
-const connectors = celoGroups({
-  chains,
+
+const { wallets } = getDefaultWallets({
+  appName: 'My RainbowKit App',
   projectId,
-  appName: (typeof document === 'object' && document.title) || 'Your App Name'
+  chains
 })
+
+const connectors = connectorsForWallets([
+  {
+    ...wallets,
+    groupName: 'CELO Only',
+    wallets: [
+      Valora({ projectId, chains }),
+      CeloWallet({ projectId, chains }),
+      CeloTerminal({ projectId, chains })
+    ]
+  },
+  {
+    groupName: 'Supports Celo',
+    wallets: [
+      metaMaskWallet({ projectId, chains }),
+      omniWallet({ projectId, chains }),
+      walletConnectWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+      coinbaseWallet({ appName: 'DGG Mint', chains })
+    ]
+  }
+])
+
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
   publicClient: publicClient
 })
-
 function MyApp ({ Component, pageProps }: any) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider theme={lightTheme({
-         accentColor: '#16A34A',
-
-      })} chains={chains}>
+      <RainbowKitProvider
+        theme={lightTheme({
+          accentColor: '#16A34A'
+        })}
+        chains={chains}
+      >
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
